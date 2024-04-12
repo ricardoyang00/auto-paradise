48970597ce19369362018368bdd6c28eaae56fac
Bruno Huang <brunohuang2012@gmail.com>
2024-04-12 15:08:28 +0100

    minor fixes

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
 
 <section id="ads">
   <article>
-    <h1><a href="index.html">Can't afford a real Ferrari? No worries! Here, you can buy even an F1 car!</a></h1>
+    <h1><a href="index.php">Can't afford a real Ferrari? No worries! Here, you can buy even an F1 car!</a></h1>
     <img src="images/ads/post-4.jpg" width="300" alt="">
   </article>
   <article>
-    <h1><a href="sell.html">Share your happiness with others by selling now!</a></h1>
+    <h1><a href="sell.php">Share your happiness with others by selling now!</a></h1>
     <img src="images/ads/post-3.jpg" width="300" alt="">
   </article>
   <article>
-    <h1><a href="index.html">Discover all the limited editions here!</a></h1>
+    <h1><a href="index.php">Discover all the limited editions here!</a></h1>
     <img src="images/ads/post-1.jpg" width="300" alt="">
   </article>  
 </section>
