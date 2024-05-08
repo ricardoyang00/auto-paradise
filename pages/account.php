2fab9444fda087258fe4d13d9d3be89effe3cbd6
Ricardo Yang <ricardo.yang00@gmail.com>
2024-05-08 19:04:51 +0100

    account style done

--- pages/account.php
+++ pages/account.php
@@ -6,6 +6,7 @@ require_once(__DIR__ . '/../utils/session.php');
 require_once(__DIR__ . '/../database/connection.db.php');
 require_once(__DIR__ . '/../database/user.class.php');
 require_once(__DIR__ . '/../templates/common.tpl.php');
+require_once(__DIR__ . '/../templates/account.tpl.php');
 
 $session = new Session();
 
@@ -21,53 +22,7 @@ $db = getDatabaseConnection();
 $user = User::getUserByUsername($db, $session->getUsername());
 $address = Address::getAddressById($db, $user->addressId);
 
-function drawProfileContent($user, $address) { ?>
-<h2>Profile</h2>
-        <div id="profile" class="account-content">
-            <div class="profile-content">
-                <a href="../actions/action_edit_profile.php" class="edit-icon"><i class="fas fa-pen"></i></a>
-
-                <h3>Username</h3>
-                <div class="data-container">
-                    <p id="username"><?=$user->username?></p>
-                </div>
-
-                <h3>Name</h3>
-                <div class="data-container">
-                    <p><?=$user->name?></p>
-                </div>
-
-                <h3>Phone Number</h3>
-                <div class="data-container">
-                    <p><?=$user->phoneNumber?></p>
-                </div>
-
-                <h3>Address</h3>
-                <div class="data-container">
-                    <p><?=$address->address?>, <?=$address->postalCode?>, <?=$address->city?>, <?=$address->country?></p>
-                </div>
-        </div>
-<?php } 
-
-function drawMyOrdersContent() { ?>
-    <h2>My Orders</h2>
-    <div id="my-orders" class="account-content">
-        <div class="orders-content">
-            <p>This is where you can display the list of orders made by the user.</p>
-            <p>You can customize this section as needed to show order details.</p>
-        </div>
-    </div>
-<?php } 
-
-function drawMySellsContent() { ?>
-    <h2>My Sells/Solds</h2>
-    <div id="my-sells" class="account-content">
-        <div class="sells-content">
-            <p>This is where you can display the list of products sold by the user.</p>
-            <p>You can customize this section as needed to show product details.</p>
-        </div>
-    </div>
-<?php } ?>
+?>
 
 <section id="account">
     <div class="account-navbar">
---
+++ templates/account.tpl.php
@@ -0,0 +1,32 @@
+<?php
+function drawProfileContent($user, $address) { ?>
+    <h2>Profile</h2>
+    <div id="profile" class="account-content">
+        <div class="profile-content">
+            <a href="../actions/action_edit_profile.php" class="edit-icon"><i class="fas fa-pen"></i></a>
+
+            <h3>Username</h3>
+            <div class="data-container">
+                <p id="username"><?=$user->username?></p>
+            </div>
+
+            <h3>Name</h3>
+            <div class="data-container">
+                <p><?=$user->name?></p>
+            </div>
+
+            <h3>Phone Number</h3>
+            <div class="data-container">
+                <p><?=$user->phoneNumber?></p>
+            </div>
+
+            <h3>Address</h3>
+            <div class="data-container">
+                <p><?=$address->address?>, <?=$address->postalCode?>, <?=$address->city?>, <?=$address->country?></p>
+            </div>
+        </div>
+        <form action="../actions/action_logout.php" method="post">
+            <button type="submit" class="profile-button" id="logout">Logout</button>
+        </form>
+    </div>
+<?php } ?>
