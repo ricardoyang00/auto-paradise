9c3504fcda4d414ec91e172387cad1df087bacaa
Bruno Huang <brunohuang2012@gmail.com>
2024-05-09 11:04:12 +0100

    fix code after merge

--- pages/buy.php
+++ pages/buy.php
@@ -13,7 +13,7 @@
     require_once(__DIR__ . '/../database/user.class.php');
     require_once(__DIR__ . '/../database/product.class.php');
     require_once(__DIR__ . '/../templates/common.tpl.php');
-    require_once(__DIR__ . '/../templates/profile.tpl.php');
+    require_once(__DIR__ . '/../templates/account.tpl.php');
     require_once(__DIR__ . '/../templates/checkout.tpl.php');
 
     drawHeader2();
