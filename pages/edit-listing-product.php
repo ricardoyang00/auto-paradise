66464854c7812390b073faf82c6db2a78c15f893
Bruno Huang <brunohuang2012@gmail.com>
2024-05-12 18:56:06 +0100

    implemented edit all details from product (excluding images)

--- pages/edit-listing-product.php
+++ pages/edit-listing-product.php
@@ -12,19 +12,29 @@
     require_once(__DIR__ . '/../database/connection.db.php');
     require_once(__DIR__ . '/../database/user.class.php');
     require_once(__DIR__ . '/../database/product.class.php');
+    require_once(__DIR__ . '/../database/category.class.php');
+    require_once(__DIR__ . '/../database/brand.class.php');
+    require_once(__DIR__ . '/../database/scale.class.php');
     require_once(__DIR__ . '/../templates/common.tpl.php');
+    require_once(__DIR__ . '/../templates/products.tpl.php');
 
     drawHeader2();
 
     $db = getDatabaseConnection();
-    
+    $categories = Category::getAllCategories($db);
+    $brands = Brand::getAllBrands($db);
+    $scales = Scale::getAllScales($db);
+
     $user = User::getUserByUsername($db, $session->getUsername());
     $address = Address::getAddressById($db, $user->addressId);
 
     $productId = $_GET['product_id'];
     $product = Product::getProductById($db, $productId);
-    ?>
+    $productCategory = Category::getCategoryById($db, $product->category);
+    $productBrand = Brand::getBrandById($db, $product->brandId);
+    $productScale = Scale::getScaleById($db, $product->scale);
 
-    <?php
+    drawEditProductForm($db, $categories, $brands, $scales, $product, $productId, $productCategory, $productBrand, $productScale);
+
     drawFooter();
 ?>
