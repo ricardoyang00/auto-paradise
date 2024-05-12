2f91ac887de452b865bd105dd5aa98bf49ce7d4c
Bruno Huang <brunohuang2012@gmail.com>
2024-05-12 14:20:46 +0100

    fixed change password

--- actions/action_change_password.php
+++ actions/action_change_password.php
@@ -12,35 +12,35 @@
     require_once(__DIR__ . '/../database/connection.db.php');
     require_once(__DIR__ . '/../database/user.class.php');
 
-    $db = getDatabaseConnection();
-    $user = User::getUserByUsername($db, $session->getUsername());
-
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $db = getDatabaseConnection();
+        $user = User::getUserByUsername($db, $session->getUsername());
+
         $oldPassword = $_POST['oldPassword'];
         $newPassword = $_POST['newPassword'];
+        $confirmNewPassword = $_POST['confirmNewPassword'];
         
-
-        if ($user->checkPassword($oldPassword)) {
-            if ($oldPassword == $newPassword) {
-                $session->addMessage('error', 'New password cannot be the same as the current password!');
-                header('Location: ' . $_SERVER['PHP_SELF']);
-                exit();
-            } else {
-                $result = $user->changePassword($db, $oldPassword, $newPassword);
-            
-                if ($result) {
-                    $session->addMessage('success', 'Password changed successfully!');
-                    header('Location: ../pages/account.php');
-                    exit();
-                } else {
-                    $session->addMessage('error', 'Failed to change password!');
-                    header('Location: ' . $_SERVER['PHP_SELF']);
-                    exit();
-                }
-            }
-        } else {
+        if (!$user->checkPassword($oldPassword)) {
             $session->addMessage('error', 'Wrong current password!');
-            header('Location: ' . $_SERVER['PHP_SELF']);
+            header('Location: ../pages/account.php');
+            exit();
+        } elseif ($newPassword !== $confirmNewPassword) {
+            $session->addMessage('error', 'New passwords do not match!');
+            header('Location: ../pages/account.php');
+            exit();
+        } elseif ($oldPassword == $newPassword) {
+            $session->addMessage('error', 'New password cannot be the same as the current password!');
+            header('Location: ../pages/account.php');
+            exit();
+        } else {
+            $result = $user->changePassword($db, $oldPassword, $newPassword);
+    
+            if ($result) {
+                $session->addMessage('success', 'Password changed successfully!');
+            } else {
+                $session->addMessage('error', 'Failed to change password!');
+            }
+            header('Location: ../pages/account.php');
             exit();
         }
     }
--- pages/content/change_password.php
+++ pages/content/change_password.php
@@ -1,19 +1,44 @@
-<h2>Profile &nbsp;&nbsp;(password)</h2>
-<div id="profile" class="account-content">
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <div class="profile-content">
-            <h3>Current Password</h3>
-            <div class="data-container">
-                <input type="password" id="oldPassword" name="oldPassword" required>
-            </div>
+<?php
+    declare (strict_types = 1);
+    
+    require_once(__DIR__ . '/../../utils/session.php');
+    require_once(__DIR__ . '/../../database/connection.db.php');
+    require_once(__DIR__ . '/../../database/user.class.php');
 
-            <h3>New Password</h3>
-            <div class="data-container">
-                <input type="password" id="newPassword" name="newPassword" required>
-            </div>
+    $section = new Session();
+    if (!$section->isLoggedIn()) {
+        header('Location: ../../pages/login.php');
+        exit();
+    }
+
+    $db = getDatabaseConnection();
+    $user = User::getUserByUsername($db, $section->getUsername());
+
+    function drawChangePasswordForm() { ?>
+        <h2>Profile &nbsp;&nbsp;(password)</h2>
+        <div id="profile" class="account-content">
+            <form method="post" action="../actions/action_change_password.php">
+                <div class="profile-content">
+                    <h3>Current Password</h3>
+                    <div class="data-container">
+                        <input type="password" id="oldPassword" name="oldPassword" required>
+                    </div>
+
+                    <h3>New Password</h3>
+                    <div class="data-container">
+                        <input type="password" id="newPassword" name="newPassword" required>
+                    </div>
+
+                    <h3>Confirm New Password</h3>
+                        <div class="data-container">
+                    <input type="password" id="confirmNewPassword" name="confirmNewPassword" required>
+                </div>
+                </div>
+                <div class="icon-and-button">
+                    <button type="submit" class="profile-button" id="change-password">Change Password</button>
+                </div>
+            </form>
         </div>
-        <div class="icon-and-button">
-            <button type="submit" class="profile-button" id="change-password">Change Password</button>
-        </div>
-    </form>
-</div>
+<?php } 
+    drawChangePasswordForm();
+?>
