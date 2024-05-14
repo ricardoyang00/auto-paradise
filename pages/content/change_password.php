f27dd58ef01024b6a995406aa4e018df42571ae9
Bruno Huang <brunohuang2012@gmail.com>
2024-05-14 15:52:09 +0100

    XSS prevent in account.tpl.php

--- pages/content/change_password.php
+++ pages/content/change_password.php
@@ -13,7 +13,7 @@
     require_once(__DIR__ . '/../../database/user.class.php');
 
     $db = getDatabaseConnection();
-    $user = User::getUserByUsername($db, $section->getUsername());
+    $user = User::getUserByUsername($db, $session->getUsername());
 
     drawChangePasswordForm();
 ?>
