d2d7b915044f90c9371c32e14704ac6fcf57fd45
Bruno Huang <brunohuang2012@gmail.com>
2024-05-15 10:23:20 +0100

    changed conditions to register and how it verifies

--- templates/account.tpl.php
+++ templates/account.tpl.php
@@ -155,11 +155,11 @@
     <div class="loginRegister">
     <form id="register-form" class="user-form" action="../actions/action_register.php" method="post">
         <label for="registerUsername">Username</label>
-        <input type="text" id="registerUsername" name="registerUsername" pattern="[A-Za-z0-9_]{3,20}" title="Username should be 3-20 characters long and can only contain letters, numbers, and underscores." required>
+        <input type="text" id="registerUsername" name="registerUsername" pattern="[A-Za-z][A-Za-z0-9_]{2,19}" title="Username should be 3-20 characters long, start with a letter, and can only contain letters, numbers, and underscores." required>
 
         <label for="registerName">Name</label>
-        <input type="text" id="registerName" name="registerName" pattern="[A-Za-z ]+" title="Name can only contain letters and spaces." maxlength="50" required>
-    
+        <input type="text" id="registerName" name="registerName" pattern="[A-Za-z][A-Za-z ]*" title="Name must start with a letter and can only contain letters and spaces." maxlength="50" required>
+
         <label for="registerPassword">Password</label>
         <input type="password" id="registerPassword" name="registerPassword" minlength="8" title="Password must be at least 8 characters long." maxlength="30" required>
     
@@ -167,17 +167,17 @@
         <input type="text" id="registerPhoneNumber" name="registerPhoneNumber" pattern="\d{9}" title="Phone number must be 9 digits long." maxlength="9" required>
 
         <label for="registerAddress">Address</label>
-        <input type="text" id="registerAddress" name="registerAddress" pattern="[A-Za-z0-9. ]+" title="Address can only contain letters, numbers, spaces and points." maxlength="60" required>
+        <input type="text" id="registerAddress" name="registerAddress" pattern="[A-Za-z][A-Za-z0-9. ]*" title="Address must start with a letter and can only contain letters, numbers, spaces, and points." maxlength="60" required>
 
         <label for="registerPostalCode">Postal Code</label>
         <input type="text" id="registerPostalCode" name="registerPostalCode" pattern="\d{4}-\d{3}" title="Postal Code must be in the format 1234-123." required>
         
         <label for="registerCity">City</label>
-        <input type="text" id="registerCity" name="registerCity" pattern="[A-Za-z ]+" title="City can only contain letters and spaces." maxlength="30" required>
+        <input type="text" id="registerCity" name="registerCity" pattern="[A-Za-z][A-Za-z ]*" title="City must start with a letter and can only contain letters and spaces." maxlength="30" required>
 
         <label for="registerCountry">Country</label>
-        <input type="text" id="registerCountry" name="registerCountry" pattern="[A-Za-z ]+" title="Country can only contain letters and spaces." maxlength="30" required>
-
+        <input type="text" id="registerCountry" name="registerCountry" pattern="[A-Za-z][A-Za-z ]*" title="Country must start with a letter and can only contain letters and spaces." maxlength="30" required>
+        
         <input type="submit" value="Register">
         <div class="account-link">
             Already have an account? <a href="profile.php">Log in</a>
